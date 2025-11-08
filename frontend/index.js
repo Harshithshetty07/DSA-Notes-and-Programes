84bc1165eec1e5877f2fe68117e14a8accc3fb76
Rakshith <harshith.shetty2002@gmail.com>
2025-11-08 10:28:40 +0530

    Update index.js

--- frontend/index.js
+++ frontend/index.js
@@ -404,3 +404,37 @@ function fourStar(n) {
          }
          let palidromeNumberRes = palidromeNumber(121);
          console.log(palidromeNumberRes)
+
+        /*  Problem 15 : Reverse Interger
+        
+        */
+
+        // Answer :
+
+        function reverseInteger(x) {
+            let XCopy = x;
+
+            x = Math.abs(x);
+            let rev = 0;
+
+            while(x > 0) {
+                let last = x % 10;
+                rev = (10 * rev) + last;
+                x = Math.floor(x / 10);
+            }
+            // if( XCopy < 0) {
+            //     return -rev
+            // } else {
+            //     return rev
+            // }
+
+            let limit = Math.pow(2, 31);
+            // let limit = 2**31        -------- Another way to calculate the power range
+
+            if(rev < -limit || rev > limit) return 0;
+
+            return (XCopy < 0) ? -rev : rev;
+        }
+
+        let reverseIntegerRes = reverseInteger(445);
+        console.log(reverseIntegerRes)
