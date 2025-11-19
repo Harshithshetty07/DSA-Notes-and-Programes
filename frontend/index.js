ed98638f4059057a7e4566bf3f5dc6d99fa67278
Rakshith <harshith.shetty2002@gmail.com>
2025-11-19 14:27:42 +0530

    Update index.js

--- frontend/index.js
+++ frontend/index.js
@@ -77,3 +77,83 @@ function sellStock(prices) {
 
 let prices = [7, 1, 4, 5, 2, 6, 3];
 console.log(sellStock(prices))
+
+
+// Merge sorted array
+
+// function mergeSorted(nums1, m, nums2, n) {
+//     let nums1Copy = nums1.slice(0,m);
+//     let p1 = 0;
+//     let p2 = 0;
+
+//     for(let i = 0; i < m + n; i++) {
+//         if( p2 >= n || (p1 < m && nums1Copy[p1] < nums2[p2]) ) {
+//             nums1[i] = nums1Copy[p1];
+//             p1++;
+//         } 
+//         else {
+//             nums1[i] = nums2[p2];
+//             p2++;
+//         }
+//     }
+//     return nums1
+// }
+
+// let nums1 = [1, 2, 3];
+// let nums2 = [2, 5, 6];
+// let m = nums1.length;
+// let n = nums2.length;
+// let mergeRes = mergeSorted(nums1, m, nums2, n)
+// console.log(mergeRes)
+
+// Or
+
+function mergeSorted(nums1, m, nums2, n) {
+    let p1 = m - 1;
+    let p2 = n - 1;
+
+    for(let i = m + n - 1; i >= 0; i--) {
+
+        if(p2 < 0) {
+            break;
+        }
+
+        if(p1 >= 0 && nums1[p1] > nums2[p2]) {
+            nums1[i] = nums1[p1];
+            p1--;
+        } else {
+            nums1[i] = nums2[p2];
+            p2--;
+        }
+    }
+    return nums1
+}
+
+let nums1 = [1, 2, 3];
+let nums2 = [2, 5, 6];
+let m = nums1.length;
+let n = nums2.length;
+let mergeRes = mergeSorted(nums1, m, nums2, n)
+console.log(mergeRes)
+
+
+// Moves zeroes  
+
+
+function moveZero(nums) {
+    let x = 0;
+
+    for(let i = 0; i < nums.length; i++) {
+        if(nums[i] !== 0) {
+            nums[x] = nums[i];
+            x++;
+        }
+    }
+    for(let i = x; i < nums.length; i++) {
+        nums[i] = 0
+    }
+    return nums
+}
+
+let nums = [1, 0, 2, 0, 3, 5]
+console.log(moveZero(nums))
