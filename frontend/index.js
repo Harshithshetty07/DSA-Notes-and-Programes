01ea52d074661ab7957534f93fee095927b83bd7
Rakshith <harshith.shetty2002@gmail.com>
2025-12-08 12:15:23 +0530

    Learning Bubble Sort Algorithm

--- frontend/index.js
+++ frontend/index.js
@@ -1,88 +1,59 @@
-// Example of Recursion
 
-function example(n) {
-  if (n == 0) return;
 
-//   console.log(n);
-  n = n - 1;
-  example(n);
+// BINARY Search
+
+let nums = [-1, 0, 3, 5, 9, 12]
+
+function binarySearch(nums, target) {
+
+
+  let left = 0;
+  let right = nums.length - 1;
+
+  while(right > left) {
+    let mid = Math.floor((left + right) / 2);
+
+    if(target === nums[mid]) {
+      return mid;
+    }
+    else if(target < mid) {
+      right = mid - 1; 
+    }
+    else {
+      left = mid + 1
+    }
+  }
+  return -1
 }
 
-let n = 5;
-example(n);
+console.log(binarySearch(nums, 9))
 
-// Example 2: Print 1 to n using recursion
-let a = 5;
 
-function exampleOne(x) {
-  if (x > a) return;
+// BUBBLE Sort
 
-//   console.log(x);
-  x = x + 1;
-  exampleOne(x); // or use this exampleOne(x++)
+let bbsort = [1, 2, 3, 5, 4];
+
+function bubbleSort(bbsort) {
+  let n = bbsort.length;
+
+  for(let i = 0; i < n - 1; i++) {
+
+    let isSwapped = false;
+
+    for(let j = 0; j < n - 1 - i; j++) {
+      if(bbsort[j] > bbsort[j + 1]) {
+        
+        let temp = bbsort[j];
+        bbsort[j] = bbsort[j + 1]
+        bbsort[j + 1] = temp;
+
+        isSwapped = true;
+      }
+    }
+      if(!isSwapped) break;
+  }
+  return bbsort;
 }
-exampleOne(1);
+let resBB = bubbleSort(bbsort)
 
-// Example 3: Sum of first n numbers
-
-function sumNum(n) {
-  if (n == 0) return 0;
-
-  return n + sumNum(n - 1);
-}
-// console.log(sumNum(5));
-
-// function sum(b) {
-//     let total = 0;
-// for(let i = 0; i < b.length; i++) {
-//    total += b[i]
-// }
-//  return total
-// }
-
-// let b = [1, 2, 3, 4, 5]
-// console.log(sum(b))
-
-
-// Example 4: Sum of all elements in an array
-let arr = [5, 3, 2, 0, 1]
-
-function sumOfArray(n) {
-    if( n == 0) return arr[n];
-
-    return arr[n] + sumOfArray( n - 1)
-
-}
-console.log(sumOfArray(arr.length - 1))
-
-// Example: Factorial using Recursion
-
-function factorial(n) {
-    if(n == 1) return 1;
-
-    return n * factorial(n - 1)
-}
-console.log(factorial(5))
-
-// Example6:  Power of Two
-
-function power(n) {
-    if( n == 1) return true;
-        else if(n < 1 || (n % 2 !==0)) return false;
-    return power(n / 2)
-}
-
-console.log(power(5 ))
-
-
-
-// Example 7: Fibonacci Number
-
-let finArray = []
-
-function fibonacci(n) {
-    if( n <= 1) return n;
-
-    return fibonacci(n - 1) + fibonacci(n - 2) 
-}
-console.log(fibonacci(23))
+console.log(resBB)
