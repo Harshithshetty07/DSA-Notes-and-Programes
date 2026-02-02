db6dcf7e091b62af0516617a775fd4290e3de3a7
Rakshith <harshith.shetty2002@gmail.com>
2026-02-02 12:00:15 +0530

    Learning linked list problems

--- frontend/index.js
+++ frontend/index.js
@@ -152,3 +152,96 @@ var reverseLinkedList = function(head) {
 
 let head2 = arrayToLinkedList([1, 2, 3, 4, 5]);  /// arrayToLinkedList is just optional 
 console.log(reverseLinkedList(head2))
+
+
+
+// Problem 4: Linked list cycle
+
+function linkedCycle(head) {
+    let seenNodes = new Set();
+    let curr = head;
+    while(curr != null) {
+        if(seenNodes.has(curr)) {
+            return true
+        }
+        seenNodes.add(curr);
+        curr = curr.next
+    }
+    return false
+}
+
+// answer 2
+
+function linkedFloyd(head) {
+
+    if(!head == null) return false
+
+    let slow = head;
+    let fast = head.next;
+    while(slow != fast) {
+        if(fast === null || fast.next == null) {
+            return false
+        }
+        slow = slow.next;
+        fast = fast.next.next
+    }
+    return true
+}
+
+// Problem 5: Palindrome linekd list 1 -> 2 -> 2 -> 1
+
+function palindromeLinked(head) {
+    // finding middle element
+    let slow = fast = head;
+    while(fast && fast.next) {
+        slow = slow.next;
+        fast = fast.next.next
+    }
+
+    // reverse the second half of list
+    let prev = null;
+    let curr = slow;
+    while(curr !== null) {
+        let temp = curr.next;
+        curr.next = prev;
+        prev = curr;
+        curr = temp
+    }
+
+    // checking for palindrome
+
+    let first = head;
+    let second = prev;
+    while(second) {
+        if(first.value !== second.value) {
+            return false
+        }
+        first = first.next;
+        second = second.next;
+    }
+    return true
+}
+
+
+// Problem 6:  Intersection of 2 linked list
+
+
+function intersecLinked(headA, headB) {
+    // put all nodes of headB inside a set
+    let store = new Set();
+
+    while(headB) {
+        store.add(headB);
+        headB = headB.next;
+    }
+
+    // Check for each element of headA if they are present in set
+
+    while(headA) {
+        if(store.has(headA)) {
+            return headA
+        }
+        headA = headA.next;
+    }
+    return null
+}
