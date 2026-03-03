4a3dce9b9c06b96575b5524d911605fed63dcfa4
Rakshith <harshith.shetty2002@gmail.com>
2026-03-03 17:21:09 +0530

    Update index.js

--- frontend/index.js
+++ frontend/index.js
@@ -31,3 +31,37 @@ function lastword(s) {
 }
 
 console.log(lastword(lword))
+
+
+// Problem 2: Find words containing character
+
+let words = ['abc', 'leet', 'code', 'aaa']
+
+// function findWordsChar(word, x) {
+//     let res = []
+//     for(let i = 0; i < word.length; i++) {
+//         if(word[i].includes(x)) {
+//             res.push(i)
+//         }
+//     }
+//     return res
+// }
+
+// console.log(findWordsChar(words, 'a'))
+
+// answer 2
+
+function findWordsChar(word, x) {
+    let res = []
+    for(let i = 0; i < word.length; i++) {
+        for(let j = 0; j < word[i].length; j++) {
+            if(word[i][j] === x) {
+                res.push(i);
+                break
+            }
+        }
+    }
+    return res
+}
+
+console.log(findWordsChar(words, 'a'))
