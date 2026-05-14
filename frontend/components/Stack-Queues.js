fd7f41f436d2f629bcc1b5bc5d2c9c8c8f1ffc8c
Rakshith <harshith.shetty2002@gmail.com>
2026-05-14 16:38:41 +0530

    Learning on

--- frontend/components/Stack-Queues.js
+++ frontend/components/Stack-Queues.js
@@ -1,420 +1,135 @@
 
-// Problem 1: Length of last word
+// Problem 1: Implement Stack using queues
 
-let lword = '  fly    to the    moon   ';
+// function myStack() {
+//     this.q1 = [];
+//     this.q2 = [];
 
-function lastword(s) {
-    /// Triim all  the  spaces at the end
+//     this.q1.push(x);
 
-    let n = s.length - 1;
-    while(n >= 0) {
-        if(s[n] === " ") {
-            n--;
-        } else {
-            break
-        }
-    }
-    // n is the point where my last word start
-        // count the length of the last word
-
-    let count = 0;
-    while(n >= 0) {
-        if(s[n] !== " ") {
-            n--;
-            count++
-        } else {
-            break
-        }
-    }
-    return count
-
-}
-
-console.log(lastword(lword))
-
-
-// Problem 2: Find words containing character
-
-let words = ['abc', 'leet', 'code', 'aaa']
-
-// function findWordsChar(word, x) {
-//     let res = []
-//     for(let i = 0; i < word.length; i++) {
-//         if(word[i].includes(x)) {
-//             res.push(i)
-//         }
+//     // Pop
+//     let n = this.q1.length;
+//     for(let i = 0; i < n - 1; i++) {
+//         let front = this.q1.shift();
+//         this.q2.push(front);
+//         //this.q2.push(this.q1.shift())
 //     }
-//     return res
+//     let ans =  this.q2.shift()
+//     let temp = this.q1;
+//     this.q1 = this.q2;
+//     this.q2 = temp
+
+//     return ans
+
+
+//     // Top
+//     let n = this.q1.length;
+//     for(let i = 0; i < n - 1; i++) {
+//         this.q2.push(this.q1.shift())
+//     }
+//     let front = this.q1[0]
+//     this.q2.push(this.q1.shift())
+//     let temp = this.q1;
+//     this.q1 = this.q2; 
+//     this.q2 = temp
+//     return front;
+
+
+//     // Empty
+
+//     return this.q1.length === 0
 // }
 
-// console.log(findWordsChar(words, 'a'))
 
-// answer 2
+// // Problem 2: Implement Queue using Stacks
 
-function findWordsChar(word, x) {
-    let res = []
-    for(let i = 0; i < word.length; i++) {
-        for(let j = 0; j < word[i].length; j++) {
-            if(word[i][j] === x) {
-                res.push(i);
-                break
+// function myQueue() {
+//     this.s1 = [];
+//     this.s2 = []
+
+//     this.s1.push(x);
+
+//     // Pop
+//     if(this.s2.length === 0) {
+//         while(this.s1.length) {
+//             this.s2.push(this.s1.pop())
+//         }
+//     }
+//     return this.s2.pop();
+
+//     // Peek
+//     if(this.s2.length === 0) {
+//         while(this.s1.length) {
+//             this.s2.push(this.s1.pop())
+//         }
+//     }
+//     return this.s2[this.s2.length - 1]
+
+//     // Empty
+//     return this.s1.length === 0 && this.s2.length === 0;
+// }
+
+
+// Problem 3: Valid Parentheses
+
+function isValid(s) {
+    let stack = []
+
+
+    let map = {
+        "{" : "}",
+        '[' : ']',
+        '(' : ')'
+    }
+
+    for(let i = 0; i < s.length; i++) {
+        if(map[s[i]]) {
+            stack.push(s[i])
+        } else {
+            let top = stack.pop();
+            if(!top || s[i] != map[top]) {
+                return false
             }
         }
     }
-    return res
-}
+    return stack.length === 0;
 
-console.log(findWordsChar(words, 'a'))
-
-
-// Problem 3: Jewels and stones
-
-let arr = 'aAAbbbb'
-
-function jewelsStones(str, val) {
-    let count = 0;
-    for(let i = 0; i < str.length; i++) {
-
-        for(let j = 0; j < val.length; j++) {
-            if(val[i] === str[i]) {
-                count++;
-                break;
-            }
-        }
-
-
-        // if(val.includes(str[i])) {
-        //     count++
-        // }
-    }
-    return count
-}
-
-// answer 2
-
-function jSet(str, val) {
-    let res = new Set(val);
-    // for(let i = 0; i< str.length; i++) {
-    //     res.add(str[i]);
+    // for(let i = 0; i < s.length; i++) {
+    //     if(s[i] === '{' || s[i] === '[' || s[i] === '(') {
+    //         stack.push(s[i])
+    //     } else {
+    //         let top = stack.pop();
+    //         if(!top || (top === '[' && s[i] != ']') || (top === '{' && s[i] != '}') || (top === '(' && s[i] != ')')) {
+    //             return false
+    //         }
+    //     }
     // }
-    let count = 0
-    for(let i = 0; i< str.length; i++) {
-        if(res.has(str[i])) {
-            count++
-        }
-    }
-    return count
+    // return stack.length === 0;
 }
 
-
-console.log(jewelsStones(arr, 'aA'))
-console.log(jSet(arr, 'aA'))
+console.log(isValid('[()]'))  // O(n)
 
 
-// Problem 4: Split a string in balanced strings
+// Problem 4: Min Stack
 
-let s = 'RLRRLLRLRL'
+function minStack() {
+    this.s = [];
 
-function splitString(s) {
-
-    let R = 0;
-    let L = 0;
-    let count = 0;
-
-    for(let i = 0; i < s.length; i++) {
-        if(s[i] === 'R') {
-            R++
-        } 
-        else {
-            L++
-        }
-        if(L === R) {
-            count++
-            R = 0;
-            L = 0;
-        }
+    // push
+    if(this.s.length === 0) {
+        this.s.push([val, val]);
+    } else {
+        let lastmin = this.s[this.s.length -1][1]
+        let minVal = Math.min(val, lastmin)
+        this.s.push([val, minVal])
     }
-    return count
+
+    // Pop
+    this.s.pop();
+
+    // Top
+    return this.s[this.s.length - 1][0]
+
+    // Get Min value
+    return this.s[this.s.length - 1][1]
 }
-
-console.log(splitString(s))
-
-
-// Problem 5: Find most frequent vowel and consonant
-
-let s1 = 'successes'
-
-function vowel(s) {
-    let map = {}  // Store all the values with freq in a map
-    for(let i = 0; i < s.length; i++) {
-        if(!map[s[i]]) {
-            map[s[i]] = 1
-        } else {
-            map[s[i]]++
-        }
-    }
-    // find the max vowels and consonant inside the map
-    let vowels = ['a', 'e', 'i', 'o', 'u']
-    let maxVowels = 0;
-    let maxConsonant = 0;
-    let keys = Object.keys(map)
-
-    for(let i = 0; i < keys.length; i++) {
-        if(vowels.includes(keys[i])) {
-            // maxVowels = Math.max(maxVowels, map[s[i]])
-            if(map[keys[i]] > maxVowels) {
-                maxVowels = map[keys[i]]
-            }
-        } else {
-            // maxConsonant = Math.max(maxConsonant, map[s[i]])
-            if(map[keys[i]] > maxConsonant) {
-                maxConsonant = map[keys[i]]
-            }
-        }
-    }
-return maxConsonant + maxVowels
-}
-
-console.log(vowel(s1))
-
-
-// Problem 6: Largest odd number in string
-
-let num = '52'
-
-function oddNumber(s) {
-    let n = s.length - 1;
-    while (n >= 0) {
-        if(Number(s[n]) % 2 == 1) {
-            return s.substring(0, n+1)
-        } else {
-            n--
-        }
-    }
-    return '';
-}
-
-console.log(oddNumber(num))
-
-
-// Problem 7: Valid Anagram
-
-let c = 'anagram';
-let t = 'nagaram'
-
-function anagram(c, t) {
-    if(c.length !== t.length) return false
-    let map = {}
-
-    for(let i = 0; i < c.length; i++) {
-        if(!map[c[i]]) {
-            map[c[i]] = 1
-        } else {
-            map[c[i]]++
-        }
-    }
-    for(let i = 0; i < t.length; i++) {
-        if(!map[t[i]] || map[t[i]] < 0) {
-            return false
-        } else {
-            map[t[i]]--
-        }
-    }
-    return true
-}
-
-console.log(anagram(c, t))
-
-
-// Problem 8: Longest common prefix
-
-let strs = ['flower', 'flow', 'flight']
-
-function commonPrefix(s) {
-    let x = 0;
-    while(x < s[0].length) {
-        let ch = s[0][x]
-        for(let i = 1; i < s.length; i++) {
-            if(ch !== s[i][x] || x == s[i].length) {
-                return s[0].substring(0, x)
-            } 
-        } 
-        x++;
-    }
-    return s[0]
-}
-
-console.log(commonPrefix(strs))
-
-
-// Problem 9: Reverse string 2
-
-let r = 'abcdefg'  // o/p k = 2 bacdfeg
-let k = 2
-
-function reverseString2(r, k) {
-    r = r.split('');
-
-    for(let x = 0; x < r.length; x =  x + ( 2 * k)) {
-        // reverse the first k elements starting from x
-
-        let n = k
-        let mid = Math.floor( n / 2);
-        for(let i = 0; i < mid; i++) {
-            let temp = r[ x + i]
-            r[x + i] = r[x + n - 1 - i]
-            r[x+ n - 1 - i] = temp
-        }
-    }
-
-        return r.join("")
-
-        // let mid = Math.floor( k / 2)
-        // for(let  i = 0; i < mid; i++) {
-        //     let temp = r[i]
-        //     r[i] = r[k - 1 - i]
-        //     r[ - 1 - i] = temp
-        // }
-
-    
-
-    // reverse a string
-    // let n = r.length;
-    // let mid = Math.floor(n / 2)
-
-    // for(let i = 0; i < mid; i++) {
-    //     let temp = r[i]
-    //     r[i] = r[n - 1 - i];
-    //     r[n - 1 - i] = temp
-    // }
-}
-
-console.log(reverseString2(r, k))
-
-
-// Problem 10: Valid palindrome - string
-
-let sts = 'A man, a plan, a canal: Panama'; 
-
-function validPalindromString(s) {
-
-    s = s.toLowerCase();
-
-    let filtered = '';
-    let rev = '';
-
-    for(let i = 0; i < s.length; i++) {
-        if(s[i].match(/[a-z0-9]/i)) {
-            filtered += s[i]
-            rev = s[i] + rev
-        }
-    }
-
-    // let rev = filtered.split("").reverse().join("");
-
-    return filtered === rev;
-}
-
-console.log(validPalindromString(sts))
-
-
-// Answer 2 --------------------- T = O(N) S = O(1)
-
-function validPalindromString2(s) {
-
-    s = s.toLowerCase()
-
-    let i = 0;
-    let j = s.length - 1;
-
-    while(i < j) {
-        if(!s[i].match(/[a-z0-9]/i)) {
-            i++;
-        } else if(!s[j].match(/[a-z0-9]/i)) {
-            j--;
-        } else if(s[i] === s[j]) {
-            i++;
-            j--;
-        } else {
-            return false
-        }
-    }
-    return true;
-}
-
-console.log(validPalindromString2(sts))
-
-
-// Problem 11: Isomorphic Strings
-
-let iso = 'foo';
-let iso1 = 'bar';
-
-function Isomorphic(s, t) {
-    if(s.length !== t.length) return false;
-
-    let mapStoT = {};
-    let mapTtoS = {};
-
-    for(let i = 0; i < s.length; i++) {
-        if(!mapStoT[s[i]] && !mapTtoS[t[i]]) {
-            mapStoT[s[i]] = t[i];
-            mapTtoS[t[i]] = s[i]
-        } else if(mapTtoS[t[i]] !== s[i]){
-            return false
-        } else if(mapStoT[s[i]] !== t[i]) {
-            return false
-        }
-    }
-    return true
-}
-
-console.log(Isomorphic(iso, iso1))
-
-
-
-// Problem 12: Group Anagram
-
-let strA = ['eat', 'tea', 'tan', 'ate', 'nat', 'bat']
-
-function groupAnagram(s) {
-    let map = {}
-    for(let i = 0; i < s.length; i++) {
-        let sortedStr = s[i].split('').sort().join('');
-        if(!map[sortedStr]) {
-            map[sortedStr] = [s[i]]
-        } else {
-            map[sortedStr].push(s[i])
-        }
-    }
-    return [...Object.values(map)]
-}
-
-console.log(groupAnagram(strA))  // T = O(n * m logm)  S = O(n * m)
-
-
-// Answer 2
-
-function groupAnagram2(s) {
-    let map = {}
-    for(let i = 0; i < s.length; i++) {
-        let arr = Array(26).fill(0);
-        let s1 = s[i];
-        for(let j = 0; j < s1.length; j++) {
-            let index = s1[j].charCodeAt()- 'a'.charCodeAt()
-            arr[index]++
-        }
-        let key = '';
-        for(let k = 0; k < 26; k++) {
-            key = key + '#' + arr[k]
-        }
-        if(!map[key]) {
-            map[key] = [s1]
-        } else {
-            map[key].push(s1)
-        }
-    }
-    return [... Object.values(map)] 
-}
-
-console.log(groupAnagram2(strA)) // T = O (n * m)  S = O(n * m)
--- frontend/index.js
+++ frontend/index.js
@@ -1,135 +1,26 @@
-
-// Problem 1: Implement Stack using queues
-
-// function myStack() {
-//     this.q1 = [];
-//     this.q2 = [];
-
-//     this.q1.push(x);
-
-//     // Pop
-//     let n = this.q1.length;
-//     for(let i = 0; i < n - 1; i++) {
-//         let front = this.q1.shift();
-//         this.q2.push(front);
-//         //this.q2.push(this.q1.shift())
-//     }
-//     let ans =  this.q2.shift()
-//     let temp = this.q1;
-//     this.q1 = this.q2;
-//     this.q2 = temp
-
-//     return ans
+// Problem 1: Sqrt(x)
+// x= 4 ------ 2
 
 
-//     // Top
-//     let n = this.q1.length;
-//     for(let i = 0; i < n - 1; i++) {
-//         this.q2.push(this.q1.shift())
-//     }
-//     let front = this.q1[0]
-//     this.q2.push(this.q1.shift())
-//     let temp = this.q1;
-//     this.q1 = this.q2; 
-//     this.q2 = temp
-//     return front;
+function mySqrt(x) {
+    if( x < 2) return x;
+    
+    let left = 2;
+    let rig = Math.floor(x / 2);
 
-
-//     // Empty
-
-//     return this.q1.length === 0
-// }
-
-
-// // Problem 2: Implement Queue using Stacks
-
-// function myQueue() {
-//     this.s1 = [];
-//     this.s2 = []
-
-//     this.s1.push(x);
-
-//     // Pop
-//     if(this.s2.length === 0) {
-//         while(this.s1.length) {
-//             this.s2.push(this.s1.pop())
-//         }
-//     }
-//     return this.s2.pop();
-
-//     // Peek
-//     if(this.s2.length === 0) {
-//         while(this.s1.length) {
-//             this.s2.push(this.s1.pop())
-//         }
-//     }
-//     return this.s2[this.s2.length - 1]
-
-//     // Empty
-//     return this.s1.length === 0 && this.s2.length === 0;
-// }
-
-
-// Problem 3: Valid Parentheses
-
-function isValid(s) {
-    let stack = []
-
-
-    let map = {
-        "{" : "}",
-        '[' : ']',
-        '(' : ')'
-    }
-
-    for(let i = 0; i < s.length; i++) {
-        if(map[s[i]]) {
-            stack.push(s[i])
+    while(left < rig) {
+        // let mid = Math.floor((left + rig) / 2);
+        let mid = Math.floor(left + (rig - left) / 2)
+        if(x === mid**2) {
+            return mid
+        }
+        else if(x < mid**2){
+            rig = mid - 1;
         } else {
-            let top = stack.pop();
-            if(!top || s[i] != map[top]) {
-                return false
-            }
+            left = mid + 1;
         }
     }
-    return stack.length === 0;
-
-    // for(let i = 0; i < s.length; i++) {
-    //     if(s[i] === '{' || s[i] === '[' || s[i] === '(') {
-    //         stack.push(s[i])
-    //     } else {
-    //         let top = stack.pop();
-    //         if(!top || (top === '[' && s[i] != ']') || (top === '{' && s[i] != '}') || (top === '(' && s[i] != ')')) {
-    //             return false
-    //         }
-    //     }
-    // }
-    // return stack.length === 0;
+    return rig
 }
 
-console.log(isValid('[()]'))  // O(n)
-
-
-// Problem 4: Min Stack
-
-function minStack() {
-    this.s = [];
-
-    // push
-    if(this.s.length === 0) {
-        this.s.push([val, val]);
-    } else {
-        let lastmin = this.s[this.s.length -1][1]
-        let minVal = Math.min(val, lastmin)
-        this.s.push([val, minVal])
-    }
-
-    // Pop
-    this.s.pop();
-
-    // Top
-    return this.s[this.s.length - 1][0]
-
-    // Get Min value
-    return this.s[this.s.length - 1][1]
-}
+console.log(mySqrt(36))
