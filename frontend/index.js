5f269fea5c727c94bbfe8112bd121e80d0afff0b
Rakshith <harshith.shetty2002@gmail.com>
2026-06-04 18:14:33 +0530

    learning on

--- frontend/index.js
+++ frontend/index.js
@@ -1,120 +1,40 @@
-// Problem 1: Sqrt(x)
-// x= 4 ------ 2
+// Problem 1: Two sum
 
+/*
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+*/
 
-function mySqrt(x) {
-    if( x < 2) return x;
-    
-    let left = 2;
-    let rig = Math.floor(x / 2);
+// answer 1: 
 
-    while(left <= rig) {
-        // let mid = Math.floor((left + rig) / 2);
-        let mid = Math.floor(left + (rig - left) / 2)
-        if(x === mid**2) {
-            return mid
-        }
-        else if(x < mid**2){
-            rig = mid - 1;
-        } else {
-            left = mid + 1;
-        }
-    }
-    return rig
-}
-
-console.log(mySqrt(36))
-
-
-// Problem 2: Guess number higher or lower
-
-function guessNumber(n) {
-    let l = 1;
-    let r = n;
-
-    while(l <= r) {
-        let mid = l + Math.floor((r - l) / 2);
-        let res = guess(mid)
-        if(res === 0) {
-            return mid
-        } else if(res < 0) {
-            r = mid - 1
-        } else {
-            l = mid + 1
-        }
-    }
-    return -1
-}
-
-// console.log(guessNumber(10))
-
-
-// Problem 3: Search in Rotated sorted array
-
-function rotateSorted(nums, target) {
-    let l = 0;
-    let r = nums.length - 1;
-    while(l <= r) {
-        let mid = l + Math.floor((r - l) / 2)
-        if(target === nums[mid]) {
-            return mid
-        }
-        // left side sorted
-        if(nums[l] <= nums[mid]) {
-            if(target < nums[mid] && target >= nums[l]) {
-                r = mid - 1
-            } else {
-                l = mid + 1
-            }
-        }
-        // Right side sorted
-        else {
-            if(target > nums[mid] && target <= nums[r]) {
-                l = mid + 1
-            } else {
-                r = mid - 1
+function twoSum(arr, target) {
+    let n = arr.length - 1;
+    for(let i = 0; i < n ; i++) {
+        for(let j = i + 1; j < n; j++) {
+            let sum = arr[i] + arr[j]
+            if(sum === target) {
+                return [i, j]
             }
         }
     }
 }
 
-console.log(rotateSorted([4, 5, 6, 7, 8, 0, 1,2,3], 1))
+console.log(twoSum([2,7,11,15], 9))
 
+// answer 2: 
 
-// Problem 4: First Bad version
+function twoSum1(arr, target) {
 
-function firstBadVersion(n, bad) {
-    let l = 1;
-    let r = n;
+let map = {}
+    for(let i = 0; i < arr.length; i++) {
+        let num = target - arr[i]
 
-    while(l < r) {
-    let m = l + Math.floor((r - l) / 2)
-        if(!bad(mid)) {
-            l = m + 1
-        } else {
-            r = m
+        if(map[num] !== undefined) {
+            return [map[num], i]
         }
+        map[arr[i]] = i;
     }
-    return r
 }
 
-console.log(firstBadVersion([1, 2, 3, 4, 5], 4))
-
-
-// Problem 5: Find Peak Element
-
-function findPeakElement(num) {
-     let l = 0
-    let r = num.length - 1;
-    while(l < r) {
-        let m = l + Math.floor((r - l) / 2)
-        if(num[m] < num[m + 1]) {
-            l = m + 1
-        } else {
-            r = m
-        }
-    }
-    return l
-}
-
-console.log(findPeakElement([1,2,1,3,5,6,4]))
+console.log(twoSum1([7, 2, 12, 16], 14))
