0236a42714aa4ef8b1bc88f160b995ca8d8646f7
Rakshith <harshith.shetty2002@gmail.com>
2026-07-02 17:20:04 +0530

    Update BinaryTree.js

--- frontend/components/BinaryTree.js
+++ frontend/components/BinaryTree.js
@@ -39,3 +39,54 @@ var preorderTraversal = function(root) {
     }
     return ans
 }
+
+
+// Problem 2: Binary Tree Inorder Traversal
+
+/*
+Input: root = [1,null,2,3]
+
+Output: [1,3,2]
+
+Input: root = [1,2,3,4,5,null,8,null,null,6,7,9]
+
+Output: [4,2,6,5,7,1,3,9,8]
+*/
+
+// answer 1 
+
+var inorderTraversal = function(root) {
+    let ans = []
+    function traversal(curr) {
+        if(!curr) return 
+        
+        traversal(curr.left)
+        ans.push(curr.val)
+
+        traversal(curr.right)
+    }
+    traversal(root)
+    return ans
+
+    
+};
+
+// answer 2
+
+var inorderTraversal = function(root) {
+    let ans = []
+    let curr = root
+    let stack = []
+
+    while(curr || stack.length) {
+        while(curr) {
+            stack.push(curr)
+            curr = curr.left
+        }
+        curr = stack.pop()
+        ans.push(curr.val)
+        curr = curr.right
+    }
+    return ans
+
+}
