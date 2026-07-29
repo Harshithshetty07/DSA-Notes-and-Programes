695ed44a3f30ae2cd80d2d5d252064ec5ceced3e
Rakshith <harshith.shetty2002@gmail.com>
2026-07-29 14:25:25 +0530

    Create BinarySearchTree.js

---
+++ frontend/components/BinarySearchTree.js
@@ -0,0 +1,11 @@
+// Problem 1: Validate Binary Search Tree
+
+/*
+Input: root = [2,1,3]
+Output: true
+
+Input: root = [5,1,4,null,null,3,6]
+Output: false
+Explanation: The root node's value is 5 but its right child's value is 4.
+*/
+
